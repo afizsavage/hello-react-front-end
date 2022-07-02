37ce6706d3f1bf5cbdb7da2cfc96d71b97232c67
Afiz Savage <savageafiz9@gmail.com>
2022-07-02 19:32:03 +0000

    setup redux

---
+++ src/components/Greeting/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Greeting = () => (
+  <div>
+    <button type="button" onClick={() => console.log('called')}>
+      Get Messages
+    </button>
+  </div>
+);
+
+export default Greeting;
